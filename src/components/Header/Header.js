0a914a8d84ed20dbcf42368ceaa3a6b68c783056
SANEDROID123 <62328357+prajwal080501@users.noreply.github.com>
2021-11-05 16:38:58 +0530

    init

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container, Logo } from './HeaderElements'
+
+const Header = () => {
+    return (
+        <Container>
+            <Logo>Covid Tracker</Logo>
+        </Container>
+    )
+}
+
+export default Header
---
+++ src/components/Header/HeaderElements.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+width: 100vw;
+height: 10vh;
+border: 2px solid black;
+align-items: center;
+`;
+
+
+export const Logo = styled.h2`
+align-items: center;
+
+`;
+
